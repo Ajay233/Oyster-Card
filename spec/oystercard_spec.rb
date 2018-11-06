4b3fc202b0602aa8acf158869c53ac47f920c1f8
Ajay <ajaymungur@hotmail.com>
2018-11-06 15:51:18 +0000

    Completed step 4 - Added balance and initialize method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
+  it "text" do
+    expect(subject).to be_instance_of(Oystercard)
+  end
+
+  it "has a balance of 0" do
+    expect(subject.balance).to eq 0
+  end
 
 end
