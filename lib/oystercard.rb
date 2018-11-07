35b4d8c332c7288751e15d4743c2be04504e6f78
Ajay <ajaymungur@hotmail.com>
2018-11-07 09:49:09 +0000

    completed step 6 - Enforced a maximum balance using private method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,21 @@
 class Oystercard
   attr_reader :balance
+  DEFAULT_LIMIT = 90
 
   def initialize(balance = 0)
     @balance = balance
+
   end
 
   def top_up(value)
+    limit_reached?(value)
     @balance += value
+  end
+
+  private
+
+  def limit_reached?(value)
+    fail "Balance limit of #{DEFAULT_LIMIT} exceeded" if (@balance + value) > DEFAULT_LIMIT
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,9 @@
 require 'oystercard'
 
-
-
 describe Oystercard do
-  let(:add_money) {subject.top_up(20)}
+  let(:add_money) { subject.top_up(20) }
+  let(:top_up_max) { subject.top_up(Oystercard::DEFAULT_LIMIT) }
+  let(:default_limit) { Oystercard::DEFAULT_LIMIT }
 
   it "text" do
     expect(subject).to be_instance_of(Oystercard)
@@ -13,13 +13,21 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-  it "can communicate with #top_up" do
-    expect(subject).to respond_to(:top_up).with(1)
-  end
+  describe "#top_up" do
 
-  it "can add to value of balance" do
-    add_money
-    expect(subject.top_up(20)).to eq 40
-  end
+    it "can communicate with #top_up" do
+      expect(subject).to respond_to(:top_up).with(1)
+    end
 
+    it "can add to value of balance" do
+      add_money
+      expect(subject.top_up(20)).to eq 40
+    end
+
+    it "will throw an error if balance exceeds 90" do
+      top_up_max
+      expect{subject.top_up(1)}.to raise_error("Balance limit of #{default_limit} exceeded")
+    end
+
+  end
 end
